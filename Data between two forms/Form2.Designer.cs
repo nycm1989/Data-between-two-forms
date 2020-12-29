ef5ec4c177a5551765cd270a4154ad858becccf7
nycm1989 <nycm1989@gmail.com>
2020-12-28 20:22:30 -0500

    1
    add label description

--- Data between two forms/Form2.Designer.cs
+++ Data between two forms/Form2.Designer.cs
@@ -31,6 +31,7 @@ namespace Data_between_two_forms
         {
             this.button1 = new System.Windows.Forms.Button();
             this.textSample2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
@@ -50,11 +51,21 @@ namespace Data_between_two_forms
             this.textSample2.Size = new System.Drawing.Size(100, 20);
             this.textSample2.TabIndex = 2;
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(167, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(114, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "fill or modify text before";
+            // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(364, 261);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textSample2);
             this.Name = "Form2";
@@ -70,5 +81,6 @@ namespace Data_between_two_forms
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textSample2;
+        private System.Windows.Forms.Label label2;
     }
 }
